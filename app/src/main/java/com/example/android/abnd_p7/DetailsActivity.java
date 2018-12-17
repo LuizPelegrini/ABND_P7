ac325a8661e29ca820217ed653479e6a9377d78e
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2018-12-17 21:56:13 -0200

    Setup new UI for the product quantity and use TextInputLayout widget for edit texts

--- app/src/main/java/com/example/android/abnd_p7/DetailsActivity.java
+++ app/src/main/java/com/example/android/abnd_p7/DetailsActivity.java
@@ -3,22 +3,42 @@ package com.example.android.abnd_p7;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.decrease_quantity_button) ImageView mDecreaseQuantityImageView;
+    @BindView(R.id.increase_quantity_button) ImageView mIncreaseQuantityImageView;
+    @BindView(R.id.quantity_text) TextView mProductQuantityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_layout);
+        ButterKnife.bind(this);
 
+        mDecreaseQuantityImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProductQuantityTextView.setText("0");
+            }
+        });
+
+        mIncreaseQuantityImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProductQuantityTextView.setText("1");
+            }
+        });
         // Gets a possible intent coming from the MainActivity
         // TODO: Query the database using a loader to retrieve info from the product
         Intent intent = getIntent();
